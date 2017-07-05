54b941864896ce3d0c8fa2daee57e70625efa9f6
Kelvin Itaboray <kelvin.itaboray@gmail.com>
2017-07-05 05:01:51 -0300

    -add get de 2 usuarios;-formatacao inicial de exibicao dos dados.

--- app.js
+++ app.js
@@ -3,23 +3,39 @@ var app = angular.module('ComparadorUsuarios', []);
 
 app.controller('comparadorUsuariosController', function($scope, $http) {
     $scope.formUsers = {};
-    $scope.usersInfo = {};
-    $scope.texto = '^[a-zA-Z ]+$';
-    $scope.numerico = '^[0-9]+$';
+    $scope.usersInfo = [];
+    $scope.validacao = '^[a-zA-Z ]+$';
     $scope.erroReq = "";
 
-    $scope.compare = function(isValid) {
-        if(isValid) {
-            var url = "https://api.github.com/users/";
-            var reqUser1 = url + $scope.formUsers.user1.modelValue;
-            var reqUser2 = url + $scope.formUsers.user2.modelValue;
-            $scope.erroReq = "";
-            $http.get(reqUser1)
+    $scope.getUser = function(url) {
+        $http.get(url)
                 .then(function (response) {
-                    $scope.usersInfo = response.data;
+                    if(response.status === 200)
+                        $scope.usersInfo.push(response.data);
+                }, function(response) {
+                    $scope.erroReq = "Dados de usuário inválidos!";
+                    $scope.usersInfo = {};
                 });
+    }
+
+    $scope.getRep = function(url) {
+    }
+
+    $scope.compareUsers = function(isValid) {
+        if(isValid) {
+            $scope.usersInfo = [];
+            $scope.erroReq = "";
+            var url = "https://api.github.com/users/";
+            var reqUser1 = url + $scope.user1;
+            var reqUser2 = url + $scope.user2;
+            $scope.getUser(reqUser1);
+            $scope.getUser(reqUser2);
         }else{
             $scope.erroReq = "Dados dos campos são inválidos!";
         }
+    }
+
+    $scope.verifyWinner = function(users){
+
     }
 });
