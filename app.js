0dca1d3249dc0e8c729f8106983c3fdfda7ea779
Kelvin Itaboray <kelvin.itaboray@gmail.com>
2017-07-05 23:33:49 -0300

    -termino da logica principal.

--- app.js
+++ app.js
@@ -1,8 +1,95 @@
 //Module
 var app = angular.module('ComparadorUsuarios', []);
 
+//Services
+app.service('pointsService', function($http, $q) {
+    this.getPoints = function(userURL, repLength, followers) {
+        var repURL = userURL+"/repos?per_page="+repLength;
+        return $http.get(repURL)
+            .then(function(response) {
+                if(response.status === 200){
+                    var points = followers*2;
+                    angular.forEach(response.data, function(rep, index) {
+                        points += parseInt(rep.stargazers_count)*5;
+                    });
+                    return points;
+                }
+            }, function(error) {
+                console.log(error);
+            });
+    }
+
+    this.isWinner = function(users, currentUser) {
+        var deferred = $q.defer();
+        var currentUserPoints = currentUser.points;
+        var isWinner = true;
+        angular.forEach(users, function(user, index){
+            if(currentUserPoints < user.points)
+                isWinner = false;
+        });
+        deferred.resolve(isWinner);
+        return deferred.promise;
+    };
+});
+
+app.service('userService', function($http, $q, pointsService) {
+    this.getUser = function(username, index) {
+        var url = "https://api.github.com/users/" + username;
+        var user = {};
+        return $http.get(url)
+            .then(function(response) {
+                if(response.status === 200){
+                    return pointsService.getPoints(url, response.data.public_repos, response.data.followers)
+                        .then(function(points) {
+                            user = {
+                                id: response.data.id,
+                                name: response.data.name,
+                                login: response.data.login,
+                                followers: response.data.followers,
+                                avatar_url: response.data.avatar_url,
+                                points: points,
+                                vencedor: false
+                            };
+                            return user;
+                        }, function(error) {
+                            console.log(error);
+                        });
+                }
+            }, function(error) {
+                console.log(error);
+            });
+    }
+
+    
+    var self = this;
+    this.getUsers = function(usernames) {
+        var deferred = $q.defer();
+        var users = [];
+        angular.forEach(usernames, function(username, index) {
+            self.getUser(username, index)
+                .then(function(response) {
+                    users.push(response);
+                    if(users.length == usernames.length){
+                        angular.forEach(users, function(user, index) {
+                            pointsService.isWinner(users, user)
+                                .then(function(response) {
+                                    user.vencedor = response;
+                                }, function(error) {
+                                    $scope.reset();
+                                });
+                        });
+                    }
+                }, function(error) {
+                    console.log(error);
+                });
+        });
+        deferred.resolve(users);
+        return deferred.promise;
+    }
+});
+
 //Controller
-app.controller('comparadorUsuariosController', function($scope, $http) {
+app.controller('comparadorUsuariosController', function($scope, $http, userService) {
     $scope.formUsers = {};
     $scope.users = [];
     $scope.validacao = '^[a-zA-Z ]+$';
@@ -10,53 +97,18 @@ app.controller('comparadorUsuariosController', function($scope, $http) {
 
     $scope.reset = function(){
         $scope.users = [];
-    }
-
-    $scope.getRep = function(userURL, index, repLength, points) {
-        var repURL = userURL+"/repos?per_page="+repLength;
-        $http.get(repURL)
-                .then(function(response) {
-                    if(response.status === 200){
-                        angular.forEach(response.data, function(rep, index) {
-                            points += parseInt(rep.stargazers_count)*5;
-                        });
-                    }
-                }, function(response) {
-                    $scope.erroReq = "Erro ao acessar repositórios: "+response.message;
-                    $scope.reset();
-                });
-    }
-
-    $scope.getUser = function(url, index) {
-        $http.get(url)
-                .then(function(response) {
-                    if(response.status === 200){
-                        var points = parseInt(response.data.followers*2);
-                        $scope.getRep(url, index, response.data.public_repos, points);
-                        $scope.users.push({
-                            id: response.data.id,
-                            name: response.data.name,
-                            login: response.data.login,
-                            followers: response.data.followers,
-                            avatar_url: response.data.avatar_url,
-                            points: points
-                        });
-                    }
-                }, function(response) {
-                    $scope.erroReq = "Dados de usuário inválidos!";
-                    $scope.reset();
-                });
+        $scope.erroReq = "";
     }
 
     $scope.compareUsers = function(isValid) {
         if(isValid) {
             $scope.reset();
-            $scope.erroReq = "";
-            var url = "https://api.github.com/users/";
-            var reqUser1 = url + $scope.user1;
-            var reqUser2 = url + $scope.user2;
-            $scope.getUser(reqUser1, 0);
-            $scope.getUser(reqUser2, 1);
+            userService.getUsers([$scope.user1, $scope.user2])
+                .then(function (response) {
+                    $scope.users = response;
+                }, function(error) {
+                    $scope.reset();
+                });
         }else{
             $scope.erroReq = "Dados dos campos são inválidos!";
         }
